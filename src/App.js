ae2f8e1b0693df02b90f657863d71fd99c5021a6
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-08 02:52:16 +0300

    add netlify folder

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Coucou ianao ry Tsanta</p>
+        <p>Bonsoir</p>
       </header>
     </div>
   );
