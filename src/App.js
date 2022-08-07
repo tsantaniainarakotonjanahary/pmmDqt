a78671e2ab8761fcb6717bcbf8ce95af1a6d113d
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-08 02:24:54 +0300

    other public

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Coucou ianao</p>
+        <p>Coucou ianao ry Tsanta</p>
       </header>
     </div>
   );
