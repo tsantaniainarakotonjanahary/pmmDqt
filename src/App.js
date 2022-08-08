896429c9c43df650646ccf12396c8bc3c8b51612
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-08 03:25:52 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+
+import { useExcelDownloder } from "react-xls";
 
 function App() {
+  const { ExcelDownloder, Type } = useExcelDownloder();
+
+  // We will make a Workbook contains 2 Worksheets
+  const data = {
+    // Worksheet named animals
+    animals: [
+      { name: "cat", category: "animal" },
+      { name: "dog", category: "animal" },
+      { name: "pig", category: "animal" },
+    ],
+    // Worksheet named pokemons
+    pokemons: [
+      { name: "pikachu", category: "pokemon" },
+      { name: "Arbok", category: "pokemon" },
+      { name: "Eevee", category: "pokemon" },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Bonsoir Tsanta Be kiraro sy akanjo sy nify</p>
-      </header>
+    <div>
+      <ExcelDownloder
+        data={data}
+        filename={"book"}
+        type={Type.Button} // or type={'button'}
+      >
+        Download
+      </ExcelDownloder>
     </div>
   );
 }
