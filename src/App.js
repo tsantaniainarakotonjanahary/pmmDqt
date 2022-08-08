c9c0a9e90efb993225a42081c4e3d2078b959aca
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-08 11:21:14 +0300

    go

--- src/App.js
+++ src/App.js
@@ -1,70 +1,107 @@
-import React from "react";
-import * as XLSX from "xlsx";
+import MyNavbar from "./MyNavbar";
+import Home from "./Home";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
-  const exportXLSX = (data, sheetName, filename) => {
-    var wb = XLSX.utils.book_new();
-    var ws = XLSX.utils.aoa_to_sheet(data);
-    XLSX.utils.book_append_sheet(wb, ws, sheetName);
-    XLSX.writeFile(wb, filename + ".xlsx");
+  const navigate = useNavigate();
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [dataUser, setDataUser] = useState({});
+  const database = [
+    {
+      username: "Nosybe",
+      password: "2021@Covax",
+      idOrgUnit: "P7ko8ftfjUy",
+    },
+    {
+      username: "Ambaja_MG",
+      password: "2021@Covax",
+      idOrgUnit: "Sc9CY4s8DWu",
+    },
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password",
   };
 
-  const getData = async (
-    domain,
-    username,
-    password,
-    periode,
-    idOrgUnit,
-    sortie,
-    outputType,
-    sort
-  ) => {
-    var data = {
-      username: username,
-      password: password,
-      periode: periode,
-      idOrgUnit: idOrgUnit,
-      sortie: sortie,
-      outputType: outputType,
-      sort: sort,
-    };
-
-    var url = new URL(domain);
-    for (let k in data) {
-      url.searchParams.append(k, data[k]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var { uname, pass } = document.forms[0];
+    const userData = database.find((user) => user.username === uname.value);
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+        setDataUser(userData);
+      }
+    } else {
+      setErrorMessages({ name: "uname", message: errors.uname });
     }
-    const response = await fetch(url, {
-      method: "GET",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
-      headers: { "Content-Type": "application/json" },
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-    });
-    return response.json();
   };
 
-  const handleClick = () => {
-    getData(
-      "https://gentle-inlet-74830.herokuapp.com/doublon-enrollement",
-      "Nosybe",
-      "2021@Covax",
-      "LAST_12_MONTHS",
-      "A8UMJuP8iI3",
-      "enrollments",
-      "ENROLLMENT",
-      "enrollmentDate"
-    ).then((data) => {
-      console.log(data.data);
-      exportXLSX(data.data, "Doublon-enrollement", "dataQT");
-    });
-  };
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const renderForm = (
+    <div className="app">
+      <div className="login-form">
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <label>Username </label>
+              <input type="text" name="uname" required />
+              {renderErrorMessage("uname")}
+            </div>
+            <div className="input-container">
+              <label>Password </label>
+              <input type="password" name="pass" required />
+              {renderErrorMessage("pass")}
+            </div>
+            <div className="button-container">
+              <input type="submit" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
-    <div>
-      <button onClick={handleClick}>Go</button>
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <MyNavbar />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            {isSubmitted
+              ? navigate("/Formulaire", {
+                  state: dataUser,
+                })
+              : renderForm}
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import Formulaire from "./Formulaire";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/Formulaire" element={<Formulaire />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
