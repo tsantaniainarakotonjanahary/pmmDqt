67e3808116f81aa19abad7a093a01f65b7067feb
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-09 08:52:09 +0300

    feefe

--- src/LoadingSpinner.js
+++ src/LoadingSpinner.js
@@ -5,6 +5,7 @@ export default function LoadingSpinner() {
   return (
     <div className="spinner-container">
       <div className="loading-spinner"></div>
+      <h1>Veuillez patientez s'il vous plait ...</h1>
     </div>
   );
 }
